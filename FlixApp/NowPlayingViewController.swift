6cd3d68b4dce5356e279787a24ec4eba1a488873
Isaac <isaac@admins-MacBook-Pro.local>
2018-09-12 02:16:42 -0700

    assignment 1 finished

---
+++ FlixApp/NowPlayingViewController.swift
@@ -0,0 +1,81 @@
+//
+//  NowPlayingViewController.swift
+//  FlixApp
+//
+//  Created by Isaac on 9/11/18.
+//  Copyright © 2018 Isaac. All rights reserved.
+//
+
+import UIKit
+import AlamofireImage
+
+class NowPlayingViewController: UIViewController, UITableViewDataSource {
+
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var tableView: UITableView!
+    var movies:[[String: Any]] = []
+    var refreshControl: UIRefreshControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector (NowPlayingViewController.didPullToRefresh(_:)), for: .valueChanged)
+        tableView.insertSubview(refreshControl, at: 0)
+        tableView.dataSource = self
+        fetchMovies()
+    
+    }
+    
+    @objc func didPullToRefresh(_ refreshControl: UIRefreshControl){
+        self.activityIndicator.startAnimating()
+        fetchMovies()
+    }
+    
+    func fetchMovies(){
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=160912ea24f0857514d870e23ab3fd12")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request){ (data, response, error) in
+            //This will run when a network request returns
+            if let error = error{
+                print(error.localizedDescription)
+            }else if let data = data {
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                print(dataDictionary)
+                let movies = dataDictionary["results"] as! [[String: Any]]
+                self.movies = movies
+                self.tableView.reloadData()
+                self.refreshControl.endRefreshing()
+                self.activityIndicator.stopAnimating()
+            }
+        }
+        task.resume()
+
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as! MovieCell
+        
+        let movie = movies[indexPath.row]
+        let title = movie["title"] as! String
+        let overview = movie["overview"] as! String
+        cell.titleLabel.text = title
+        cell.overviewLabel.text = overview
+        
+        let posterPathString = movie["poster_path"] as! String
+        let baseURLString = "https://image.tmdb.org/t/p/w500"
+        let posterURL = URL(string: baseURLString + posterPathString)!
+        cell.posterImageView.af_setImage(withURL: posterURL)
+        
+        return cell
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
